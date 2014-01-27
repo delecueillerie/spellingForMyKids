86560c89862d5e674ee662992140fd8bb070bceb
Lagspoon <lagspoon@gmail.com>
2014-01-27 12:35:32 +0100

    1st

---
+++ spellingForMyKids/SPTestPageVC.h
@@ -0,0 +1,16 @@
+//
+//  SPTestPageVC.h
+//  spellingForMyKids
+//
+//  Created by Olivier Delecueillerie on 23/01/2014.
+//  Copyright (c) 2014 Olivier Delecueillerie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPTestPageVC : UIViewController
+
+
+@property (nonatomic, strong) NSString *inputWord;
+
+@end
